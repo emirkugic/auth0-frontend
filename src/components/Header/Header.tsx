79ede81d85226ac6671a667ffdd512f2a860bcdc
emirkugic <emir.kugic@hotmail.com>
2024-03-07 21:39:57 +0100

    responsive wip

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,64 +9,64 @@ import Box from "@mui/material/Box";
 import auth0LogoDark from "../../assets/logos/authLogo/auth0-dark.svg";
 import classes from "./Header.module.css";
 import { useState } from "react";
-import { HeaderMenu } from '../HeaderMenu';
-import { Typography } from '@mui/material';
+import { HeaderMenu } from "../HeaderMenu";
+import { Typography } from "@mui/material";
 import { stringAvatar } from "../../utils";
 import { ReduxHooks } from "../../hooks";
 import { selectUser } from "../../store/slice/userSlice";
 
 function ResponsiveAppBar() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
 
-  const user = ReduxHooks.useAppSelector(selectUser)
-  if (!user) return null;
+	const user = ReduxHooks.useAppSelector(selectUser);
+	if (!user) return null;
 
-  const { firstName, lastName } = user;
+	const { firstName, lastName } = user;
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <AppBar position="static" elevation={1} className={classes["app-bar"]}>
-      <Container
-        className={classes["app-bar__content"]}
-        maxWidth={false}
-        disableGutters
-      >
-        <Toolbar disableGutters>
-          <Box className={classes["app-bar__logo"]}>
-            <img src={auth0LogoDark} alt="Skim Technologies Logo" />
-          </Box>
+	return (
+		<AppBar position="static" elevation={1} className={classes["app-bar"]}>
+			<Container
+				className={classes["app-bar__content"]}
+				maxWidth={false}
+				disableGutters
+			>
+				<Toolbar disableGutters>
+					<Box className={classes["app-bar__logo"]}>
+						<img src={auth0LogoDark} alt="Skim Technologies Logo" />
+					</Box>
 
-          <Box className={classes["app-bar__avatar"]}>
-            <Tooltip title="Open settings">
-              <IconButton sx={{ p: 0 }} onClick={handleClick}>
-                <Avatar
-                  alt={`${firstName} ${lastName}`}
-                  src="https://images.mubicdn.net/images/cast_member/830947/cache-738230-1638187722/image-w856.jpg?size=800x"
-                  {...stringAvatar(`${firstName} ${lastName}`)}
-                />
-              </IconButton>
-            </Tooltip>
-            <Typography
-              className={classes["app-bar__avatar__title"]}
-              variant="h6"
-              component="span"
-            >
-              {`Hello, ${firstName}!`}
-            </Typography>
-          </Box>
-        </Toolbar>
-      </Container>
+					<Box className={classes["app-bar__avatar"]}>
+						<Tooltip title="Open settings">
+							<IconButton sx={{ p: 0 }} onClick={handleClick}>
+								<Avatar
+									alt={`${firstName} ${lastName}`}
+									src="https://images.mubicdn.net/images/cast_member/830947/cache-738230-1638187722/image-w856.jpg?size=800x"
+									{...stringAvatar(`${firstName} ${lastName}`)}
+								/>
+							</IconButton>
+						</Tooltip>
+						<Typography
+							className={classes["app-bar__avatar__title"]}
+							variant="h6"
+							component="span"
+						>
+							{`Hello, ${firstName}!`}
+						</Typography>
+					</Box>
+				</Toolbar>
+			</Container>
 
-      <HeaderMenu anchorEl={anchorEl} open={open} handleClose={handleClose} />
-    </AppBar>
-  );
+			<HeaderMenu anchorEl={anchorEl} open={open} handleClose={handleClose} />
+		</AppBar>
+	);
 }
 export default ResponsiveAppBar;
--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -5,21 +5,21 @@ import { ServiceCard } from "../../components/ServiceCard";
 import classes from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  const numberOfServiceCards = 12;
+	const numberOfServiceCards = 12;
 
-  return (
-    <Box className={classes.container}>
-      <Header />
-      <DashDrawer />
-      <Box className={classes["container__service-cards"]}>
-        <Box className={classes["grid-container"]}>
-          {[...Array(numberOfServiceCards)].map((_, index) => (
-            <ServiceCard key={index} />
-          ))}
-        </Box>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box className={classes.container}>
+			<Header />
+			<DashDrawer />
+			<Box className={classes["container__service-cards"]}>
+				<Box className={classes["grid-container"]}>
+					{[...Array(numberOfServiceCards)].map((_, index) => (
+						<ServiceCard key={index} />
+					))}
+				</Box>
+			</Box>
+		</Box>
+	);
 };
 
 export default Dashboard;
