c23ebd500e27f155ac3f1117f1d41255150704b0
FiggyHunter <leonardo.roi3@gmail.com>
2024-03-15 15:20:41 +0100

    overhauled logic on opening the tutorial modal

--- src/components/DashDrawer/DashDrawer.tsx
+++ src/components/DashDrawer/DashDrawer.tsx
@@ -1,8 +1,15 @@
-import { Accordion, AccordionDetails, AccordionSummary, IconButton, Paper, Typography } from "@mui/material";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faListCheck } from '@fortawesome/free-solid-svg-icons'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  IconButton,
+  Paper,
+  Typography,
+} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faListCheck } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import ListItemText from "@mui/material/ListItemText";
 import { useState, useEffect, useRef } from "react";
 import ListItem from "@mui/material/ListItem";
@@ -15,8 +22,11 @@ import classes from "./DashDrawer.module.css";
 
 const DashDrawer = () => {
   const [open, setOpen] = useState(false);
-  const [expandedAccordion, setExpandedAccordion] = useState<string | false>(false);
+  const [expandedAccordion, setExpandedAccordion] = useState<string | false>(
+    false
+  );
   const drawerRef = useRef<HTMLDivElement>(null);
+
   const { data } = useTasks();
 
   useEffect(() => {
@@ -38,55 +48,62 @@ const DashDrawer = () => {
     };
   }, []);
 
-  const handleAccordionChange = (panel: string) => (
-    _event: React.ChangeEvent<{}>,
-    isExpanded: boolean
-  ) => {
-    setExpandedAccordion(isExpanded ? panel : false);
-  };
+  const handleAccordionChange =
+    (panel: string) => (_event: React.ChangeEvent<{}>, isExpanded: boolean) => {
+      setExpandedAccordion(isExpanded ? panel : false);
+    };
 
   const list = (
-    <Box
-      className={classes.container__drawer__list}
-      role="presentation"
-    >
+    <Box className={classes.container__drawer__list} role="presentation">
       <Box className={classes.container__drawer__title}>
-        <FontAwesomeIcon className={classes["container__drawer__title-icon"]} icon={faListCheck as IconProp} />
-        <Typography variant="h6" className={classes["container__drawer__title-text"]}>
+        <FontAwesomeIcon
+          className={classes["container__drawer__title-icon"]}
+          icon={faListCheck as IconProp}
+        />
+        <Typography
+          variant="h6"
+          className={classes["container__drawer__title-text"]}
+        >
           Tasks
         </Typography>
       </Box>
       <List>
-        {data && data.map((task: Task, index: number) => (
-          <Accordion
-            key={index}
-            className={classes.accordian}
-            elevation={0}
-            expanded={expandedAccordion === `panel${index + 1}`}
-            onChange={handleAccordionChange(`panel${index + 1}`)}
-          >
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon />}
-              aria-controls={`panel${index + 1}-content`}
-              id={`panel${index + 1}-header`}
+        {data &&
+          data.map((task: Task, index: number) => (
+            <Accordion
+              key={index}
+              className={classes.accordian}
+              elevation={0}
+              expanded={expandedAccordion === `panel${index + 1}`}
+              onChange={handleAccordionChange(`panel${index + 1}`)}
             >
-              <Typography>{task.name}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <List>
-                {task.subtasks.map((subtask: Subtask, subIndex: number) => (
-                  <ListItem
-                    className={`${classes.subtask} ${subtask.status ? classes.completed : ""}`}
-                    key={subIndex}
-                    disablePadding
-                  >
-                    <ListItemText className={classes.subtask__text} primary={`${subtask.name}`} />
-                  </ListItem>
-                ))}
-              </List>
-            </AccordionDetails>
-          </Accordion>
-        ))}
+              <AccordionSummary
+                expandIcon={<ArrowDropDownIcon />}
+                aria-controls={`panel${index + 1}-content`}
+                id={`panel${index + 1}-header`}
+              >
+                <Typography>{task.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                  {task.subtasks.map((subtask: Subtask, subIndex: number) => (
+                    <ListItem
+                      className={`${classes.subtask} ${
+                        subtask.status ? classes.completed : ""
+                      }`}
+                      key={subIndex}
+                      disablePadding
+                    >
+                      <ListItemText
+                        className={classes.subtask__text}
+                        primary={`${subtask.name}`}
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              </AccordionDetails>
+            </Accordion>
+          ))}
       </List>
     </Box>
   );
@@ -95,7 +112,9 @@ const DashDrawer = () => {
     <Box className={classes.container}>
       <IconButton
         className={classes.container__action}
-        onClick={() => setOpen(true)}
+        onClick={() => {
+          setOpen((prevOpen) => !prevOpen);
+        }}
       >
         <FontAwesomeIcon icon={faListCheck as IconProp} />
       </IconButton>
@@ -107,6 +126,6 @@ const DashDrawer = () => {
       </Paper>
     </Box>
   );
-}
+};
 
 export default DashDrawer;
